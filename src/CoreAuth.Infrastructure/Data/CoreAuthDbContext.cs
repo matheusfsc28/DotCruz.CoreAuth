0fa543d0a054ae64ca6cb37b2576a7a0c443f226
Matheus Felipe <matheus.felipescruz@gmail.com>
2026-03-09 16:19:26 -0300

    FEAT: Adding CoreAuthDbContext

---
+++ src/CoreAuth.Infrastructure/Data/CoreAuthDbContext.cs
@@ -0,0 +1,47 @@
+﻿using CoreAuth.Domain.Entities.Base;
+using CoreAuth.Domain.Entities.Tokens;
+using CoreAuth.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace CoreAuth.Infrastructure.Data
+{
+    public class CoreAuthDbContext : DbContext
+    {
+        public CoreAuthDbContext(DbContextOptions<CoreAuthDbContext> options) : base(options) { }
+    
+        public DbSet<PasswordResetToken> PasswordResetTokens { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CoreAuthDbContext).Assembly);
+
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes()
+                .Where(e => e.ClrType != null && typeof(BaseEntity).IsAssignableFrom(e.ClrType)))
+            {
+                var parameter = Expression.Parameter(entityType.ClrType, "e");
+                var propertyAccess = Expression.Property(parameter, nameof(BaseEntity.DeletedAt));
+                var nullValue = Expression.Constant(null, typeof(DateTime?));
+                var expression = Expression.Equal(propertyAccess, nullValue);
+                var lambda = Expression.Lambda(expression, parameter);
+                modelBuilder.Entity(entityType.ClrType).HasQueryFilter(lambda);
+            }
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Entity.Touch();
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
