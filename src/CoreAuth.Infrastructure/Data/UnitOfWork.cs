a660f492448a803d011fc132511e5ba1b587c300
Matheus Felipe <matheus.felipescruz@gmail.com>
2026-03-26 21:40:35 -0300

    FEAT: Adding unit of work

---
+++ src/CoreAuth.Infrastructure/Data/UnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using CoreAuth.Domain.Interfaces.Data;
+
+namespace CoreAuth.Infrastructure.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly CoreAuthDbContext _context;
+
+        public UnitOfWork(CoreAuthDbContext context) => _context = context;
+
+        public Task CommitAsync(CancellationToken cancellationToken = default)
+        {
+            return _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
