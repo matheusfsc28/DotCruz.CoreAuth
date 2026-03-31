018109c4f13030149b9e98e35be9715256632814
Matheus Felipe <matheus.felipescruz@gmail.com>
2026-03-30 22:48:45 -0300

    FEAT: Adding tokens repositories

---
+++ src/CoreAuth.Infrastructure/Repositories/Tokens/PasswordResetTokenRepository.cs
@@ -0,0 +1,12 @@
+﻿using CoreAuth.Domain.Entities.Tokens;
+using CoreAuth.Domain.Interfaces.Repositories.Tokens;
+using CoreAuth.Infrastructure.Data;
+using CoreAuth.Infrastructure.Repositories.Base;
+
+namespace CoreAuth.Infrastructure.Repositories.Tokens
+{
+    public class PasswordResetTokenRepository : BaseRepository<PasswordResetToken>, IPasswordResetTokenReadRepository, IPasswordResetTokenWriteRepository
+    {
+        public PasswordResetTokenRepository(CoreAuthDbContext context) : base(context) { }
+    }
+}
---
+++ src/CoreAuth.Infrastructure/Repositories/Tokens/RefreshTokenRepository.cs
@@ -0,0 +1,12 @@
+﻿using CoreAuth.Domain.Entities.Tokens;
+using CoreAuth.Domain.Interfaces.Repositories.Tokens;
+using CoreAuth.Infrastructure.Data;
+using CoreAuth.Infrastructure.Repositories.Base;
+
+namespace CoreAuth.Infrastructure.Repositories.Tokens
+{
+    public class RefreshTokenRepository : BaseRepository<RefreshToken>, IRefreshTokenReadRepository, IRefreshTokenWriteRepository
+    {
+        public RefreshTokenRepository(CoreAuthDbContext context) : base(context) { }
+    }
+}
